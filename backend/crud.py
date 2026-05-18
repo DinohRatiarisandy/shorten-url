9aaf20ff21e92eb16f24cdcc34f647f285d7eab3
Dinoh Ratiarisandy <dinohratiarisandy@gmail.com>
2026-05-18 21:32:28 -0200

    feat: use hashids rather than id created randomly

--- backend/crud.py
+++ backend/crud.py
@@ -1,20 +1,26 @@
-import random
-import string
+import os
 
+import dotenv
+from hashids import Hashids
 from sqlalchemy.orm import Session
 
 import models
 import schemas
 
+dotenv.load_dotenv(".env", override=True)
+hashids_salt_key = os.getenv("HASHID_SALT_SECRET")
+hashids = Hashids(salt=hashids_salt_key or "dinoh-liantsoa-ratiarisandy", min_length=6)
 
-def generate_short_code(length: int = 6) -> str:
-    """Génère un code court aléatoire (ex: 'aBc123')"""
-    chars = string.ascii_letters + string.digits
-    return "".join(random.choice(chars) for _ in range(length))
+
+def generate_short_code(next_id: int) -> str:
+    """Génère un code court unique à partir d'un ID."""
+    return hashids.encode(next_id)
 
 
 def create_link(db: Session, link: schemas.LinkCreate):
-    short_code = generate_short_code()
+    last_link = db.query(models.Link).order_by(models.Link.id.desc()).limit(1).scalar()
+    next_id = (last_link.id + 1) if last_link else 1
+    short_code = generate_short_code(next_id)
     db_link = models.Link(original_url=link.original_url, short_code=short_code)
     db.add(db_link)
     db.commit()
