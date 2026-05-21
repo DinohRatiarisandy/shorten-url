2d2dfa3995cc454dc4faee37621458a5adf6ff01
Dinoh Ratiarisandy <dinohratiarisandy@gmail.com>
2026-05-21 20:20:52 -0200

    feat: move to postrgresql

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,22 @@
+import os
+
+import dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./shorten_url.db"
+dotenv.load_dotenv(override=True)
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+POSTGRES_DATABASE_URL = os.getenv("POSTGRES_DATABASE_URL")
+SQLITE3_DATABASE_URL = "sqlite:///./shorten_url.db"
+
+if not POSTGRES_DATABASE_URL:
+    engine = create_engine(
+        SQLITE3_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(POSTGRES_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
