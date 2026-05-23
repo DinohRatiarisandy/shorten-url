f3bf215a8c910859d6b45602282ec3f2d25e3b55
Dinoh Ratiarisandy <dinohratiarisandy@gmail.com>
2026-05-23 18:52:13 -0200

    feat: url shortener hook

---
+++ frontend/src/composables/useUrlShortener.ts
@@ -0,0 +1,59 @@
+import { ref } from "vue";
+import { createShortLink } from "@/services/linkService";
+import { isValidUrl, getHostname } from "@/utils/urlHelper";
+import { API_HOSTNAME } from "@/services/linkService";
+
+export function useUrlShortener() {
+	const url = ref("");
+	const shortUrl = ref("");
+	const loading = ref(false);
+	const errorMessage = ref("");
+	const lastSubmittedUrl = ref("");
+
+	async function submit() {
+		errorMessage.value = "";
+
+		if (getHostname(url.value) == API_HOSTNAME) {
+			errorMessage.value =
+				"You can't shorten an already shortened link. 😒";
+			return;
+		}
+		if (!url.value) {
+			errorMessage.value = "Please enter a URL.";
+			return;
+		}
+		if (!isValidUrl(url.value)) {
+			errorMessage.value = "Invalid URL.";
+		}
+		if (url.value === lastSubmittedUrl.value) {
+			errorMessage.value = "This url was already shortened";
+			return;
+		}
+
+		try {
+			loading.value = true;
+			errorMessage.value = "";
+			shortUrl.value = await createShortLink(url.value);
+			lastSubmittedUrl.value = url.value;
+		} catch (error: any) {
+			errorMessage.value = error.message || "Network error.";
+		} finally {
+			loading.value = false;
+		}
+	}
+
+	function clearInput() {
+		url.value = "";
+		errorMessage.value = "";
+	}
+
+	return {
+		url,
+		shortUrl,
+		loading,
+		errorMessage,
+		lastSubmittedUrl,
+		submit,
+		clearInput,
+	};
+}
