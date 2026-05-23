a1f194c3aedfa2ea205f8de37cc926e9bccc53f9
Dinoh Ratiarisandy <dinohratiarisandy@gmail.com>
2026-05-23 21:03:02 -0200

    feat: send custom code into backend

--- frontend/src/services/linkService.ts
+++ frontend/src/services/linkService.ts
@@ -1,15 +1,19 @@
-const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL;
 export const API_HOSTNAME = new URL(API_URL).hostname;
 
-export async function createShortLink(original_url: string) {
+export async function createShortLink(
+	original_url: string,
+	customCode?: string,
+) {
+	const body: any = { original_url };
+	if (customCode) body.custom_short_code = customCode;
+
 	const response = await fetch(`${API_URL}/links/`, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
 		},
-		body: JSON.stringify({
-			original_url: original_url,
-		}),
+		body: JSON.stringify(body),
 	});
 
 	const data = await response.json();
