5152c107b5fe8903699f6dc9216fc17538847b9c
Dinoh Ratiarisandy <dinohratiarisandy@gmail.com>
2026-05-25 16:19:14 -0200

    feat: create access token

--- backend/auth/security.py
+++ backend/auth/security.py
@@ -1,4 +1,15 @@
+import os
+from datetime import UTC, datetime, timedelta
+
 import bcrypt
+import dotenv
+import jwt
+
+dotenv.load_dotenv(override=True)
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = float(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60.0))
 
 
 def hash_password(password: str):
@@ -9,3 +20,13 @@ def hash_password(password: str):
 
 def verify_password(user_pass: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(user_pass.encode("utf-8"), hashed_password.encode("utf-8"))
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(UTC) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(payload=to_encode, key=SECRET_KEY, algorithm=ALGORITHM)
