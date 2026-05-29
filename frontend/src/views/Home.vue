dbb367249cb9d166e1f1d7a2a18eb4909857c3dc
Dinoh Ratiarisandy <dinohratiarisandy@gmail.com>
2026-05-29 19:43:21 -0200

    fix: delete link navigate to admin

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -34,7 +34,6 @@ function onInputChange() {
 
 <template>
 	<router-view />
-	<router-link to="/admin/login"> Admin </router-link>
 	<div class="min-h-screen flex items-center justify-center bg-gray-50">
 		<div class="w-105 p-6 shadow-xl rounded-2xl bg-white">
 			<!-- Title -->
