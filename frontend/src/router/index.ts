ac9db447261efbd90700e0b3fddc488ccc4114ed
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2026-06-10 06:38:53 +0300

    chore: activate the editor format

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,39 +2,39 @@ import { createRouter, createWebHistory } from "vue-router";
 import { getMe } from "@/admin/services/auth";
 
 const routes = [
-	{
-		path: "/",
-		component: () => import("@/views/Home.vue"),
-	},
+    {
+        path: "/",
+        component: () => import("@/views/Home.vue"),
+    },
 
-	{
-		path: "/admin/login",
-		component: () => import("@/admin/components/AdminLogin.vue"),
-	},
+    {
+        path: "/admin/login",
+        component: () => import("@/admin/components/AdminLogin.vue"),
+    },
 
-	{
-		path: "/admin/dashboard",
-		component: () => import("@/admin/components/AdminDashboard.vue"),
-		meta: { requiresAuth: true },
-	},
+    {
+        path: "/admin/dashboard",
+        component: () => import("@/admin/components/AdminDashboard.vue"),
+        meta: { requiresAuth: true },
+    },
 ];
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes,
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
 });
 
 router.beforeEach(async (to) => {
-	if (to.meta.requiresAuth) {
-		try {
-			await getMe();
-			return true;
-		} catch {
-			return "/admin/login";
-		}
-	}
+    if (to.meta.requiresAuth) {
+        try {
+            await getMe();
+            return true;
+        } catch {
+            return "/admin/login";
+        }
+    }
 
-	return true;
+    return true;
 });
 
 export default router;
